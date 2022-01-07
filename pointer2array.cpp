2e8c4d27d5d367c57bab324bb77026fb02d88afc
anikethhebbar <76802363+anikethhebbar@users.noreply.github.com>
2022-01-07 19:03:06 +0530

    2nd pg commit

--- pointer2array.cpp
+++ pointer2array.cpp
@@ -1,9 +1,16 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 int main()
 {
-    
-    return 0;
+    int A[5] = {2, 4, 6, 8, 10};
+    int *p;
+    p = A;
 
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i];
+    }
+
+    return 0;
 }
