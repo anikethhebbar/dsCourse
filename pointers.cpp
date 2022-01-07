a392b98c330ca05191c93988af56ee9daaf0bc87
anikethhebbar <76802363+anikethhebbar@users.noreply.github.com>
2022-01-07 17:40:47 +0530

    formatted code

--- pointers.cpp
+++ pointers.cpp
@@ -1,15 +1,14 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 int main()
 {
-    int a= 10;
+    int a = 10;
     int *p;
     p = &a;
 
-    cout<<a<<endl;
+    cout << a << endl;
     printf("USING POINTER %d", *p);
 
     return 0;
-
 }
