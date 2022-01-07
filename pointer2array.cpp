19dfc14117d9d983c9584eafc1ad76ce354efb39
anikethhebbar <76802363+anikethhebbar@users.noreply.github.com>
2022-01-07 19:13:29 +0530

    accessing heap code

--- pointer2array.cpp
+++ pointer2array.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <stdio.h>
+#include<stdlib.h>
+
 using namespace std;
 int main()
+    
 {
-    int A[5] = {2, 4, 6, 8, 10};
     int *p;
-    p = A;
-
+    p=(int *)malloc(5*sizeof(int)); //{p = new int [5]} for cpp 
+      
+    p[0] = 12;
+    p[1] = 54;
+    p[2] = 34;
+    p[3] = 44;
+    p[4] = 14;
+    
     for (int i = 0; i < 5; i++)
     {
-        cout << A[i];
+        cout << p[i] <<endl;
     }
 
+    free(p);//delete [] p;
     return 0;
+    
 }
